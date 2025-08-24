c3c41da619b7520a6ee5a5bfccec20af49abc7af
hovmel <hovmel2016@gmail.com>
2025-08-24 21:52:11 +0400

    version after updating pods

--- VMIGrent/API/Rest/AuthAPI.swift
+++ VMIGrent/API/Rest/AuthAPI.swift
@@ -5,56 +5,38 @@
 //  Created by Mikhail Koroteev on 05.06.2022.
 //
 
-import Foundation
 import Moya
 
-enum AuthAPI: TargetType, BaseAPI {
+enum AuthAPI: BaseAPI {
     case sendPhone(String)
     case checkPhone(String, String)
-    
-    // MARK: - TargetType & BaseAPI
-    
-    var baseURL: URL { URLProvider.shared.httpURL }
-    
+}
+
+extension AuthAPI: TargetType {
     var path: String {
         switch self {
-        case .sendPhone:
-            return "/customers/self/auth/sms-send"
-        case .checkPhone:
-            return "/customers/self/auth/sms-check"
+            case .sendPhone:
+                return "/customers/self/auth/sms-send"
+            case .checkPhone:
+                return "/customers/self/auth/sms-check"
         }
     }
     
     var method: Moya.Method {
-        return .post
+        switch self {
+            case .sendPhone, .checkPhone:
+            return .post
+        }
     }
     
     var task: Task {
         switch self {
-        case .sendPhone(let phone):
-            return .requestParameters(parameters: ["phone": phone], encoding: JSONEncoding.default)
-        case .checkPhone(let code, let phone):
-            return .requestParameters(parameters: ["code": code, "phone": phone], encoding: JSONEncoding.default)
+            case .sendPhone(let phone):
+                let params: [String: Any] = ["phone": phone]
+                return .requestParameters(parameters: params, encoding: JSONEncoding.default)
+            case .checkPhone(let code, let phone):
+                let params: [String: Any] = ["code": code, "phone": phone]
+                return .requestParameters(parameters: params, encoding: JSONEncoding.default)
         }
     }
-    
-    var headers: [String: String]? {
-        if let token = AuthManager.shared.getSessionToken() {
-            return [
-                "Content-Type": "application/json",
-                "Authorization": token,
-                "Accept-Language": Locale.current.identifier,
-                "Accept": "application/json"
-            ]
-        } else {
-            return [
-                "Content-Type": "application/json",
-                "Accept-Language": Locale.current.identifier,
-                "Accept": "application/json"
-            ]
-        }
-    }
-    
-    var sampleData: Data { Data() }
 }
-
